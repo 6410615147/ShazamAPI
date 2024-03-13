8561ab490a060c01f278831f3b86bba2c2ca0f3e
6410615147 <6410615147@student.tu.ac.th>
2024-03-13 15:10:29 +0700

    Initial Commit

---
+++ ShazamAPI/ShazamAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamAPIApp.swift
+//  ShazamAPI
+//
+//  Created by Babypowder on 13/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ShazamAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
